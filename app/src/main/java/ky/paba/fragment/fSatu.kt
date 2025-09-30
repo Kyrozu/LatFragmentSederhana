a3883200e375abf8aa1d40ec4a25079b75361986
Kyrozu <nat80222@gmail.com>
2025-09-30 18:30:27 +0700

    btnOpenFSatu

--- app/src/main/java/ky/paba/fragment/fSatu.kt
+++ app/src/main/java/ky/paba/fragment/fSatu.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,6 +27,22 @@ class fSatu : Fragment() {
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val _btnOpenFSatu = view.findViewById<Button>(R.id.btnOpenFSatu)
+
+        _btnOpenFSatu.setOnClickListener {
+            val fragmentManager = parentFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+
+            val fragmentDua = fDua()
+
+            fragmentTransaction.replace(R.id.fragment_container, fragmentDua)
+            fragmentTransaction.commit()
         }
     }
 
